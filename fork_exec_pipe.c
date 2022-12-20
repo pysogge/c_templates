430a5adf94140cd7935f14fc12fe7cde2768f971
pysogge <x@pysogge.com>
2022-12-19 20:09:53 -0800

    read pipe

--- fork_exec_pipe.c
+++ fork_exec_pipe.c
@@ -13,22 +13,44 @@ int main(int argc, char** argv) {
     pid_t pid = fork();
 
     if (pid == 0) {
-        // This is the child process. Close the write end of the pipe.
-        close(pipefd[1]);
+        // // This is the child process. Close the write end of the pipe.
+        // close(pipefd[1]);
 
-        // Connect the read end of the pipe to the child's standard input.
-        dup2(pipefd[0], STDIN_FILENO);
+        // // Connect the read end of the pipe to the child's standard input.
+        // dup2(pipefd[0], STDIN_FILENO);
+
+        // This is the child process. Close the read end of the pipe.
+        close(pipefd[0]);
+
+        // Connect the write end of the pipe to the child's standard output.
+        dup2(pipefd[1], STDOUT_FILENO);
 
         // Execute the new program.
         execl("/path/to/program", "program_name", NULL);
     } else if (pid > 0) {
-        // This is the parent process. Close the read end of the pipe.
-        close(pipefd[0]);
+        // // This is the parent process. Close the read end of the pipe.
+        // close(pipefd[0]);
 
         // Connect the write end of the pipe to the parent's standard output.
-        dup2(pipefd[1], STDOUT_FILENO);
+        // dup2(pipefd[1], STDOUT_FILENO);
 
-        // Wait for the child process to finish.
+        // CLose the write end of the pipe.
+        close(pipefd[1]);
+
+        // While there is data, read a lien from the pipe and print it to the screen, but after five lines, break
+        char buf[1024];
+        int i = 0;
+        while (read(pipefd[0], buf, sizeof(buf)) > 0) {
+            printf("%s", buf);
+            if (++i == 5) {
+                break;
+            }
+        }
+
+        // kill the child process
+        kill(pid, SIGKILL);
+
+        // Or Wait for the child process to finish.
         int status;
         wait(&status);
     } else {
